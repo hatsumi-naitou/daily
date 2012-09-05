6c994d28436fba12deada4c3750ed4acb094ccdf
h.n <hatsumi.naitou@gmail.com>
2012-09-06 01:09:36 +0900

    0905

--- 0905/gen/com/example/ict/R.java
+++ 0905/gen/com/example/ict/R.java
@@ -28,11 +28,11 @@ public final class R {
     }
     public static final class id {
         public static final int button1=0x7f080003;
-        public static final int imageView2=0x7f080002;
+        public static final int imageView2=0x7f080001;
         public static final int imageView4=0x7f080007;
         public static final int menu_settings=0x7f080008;
         public static final int tableRow1=0x7f080000;
-        public static final int tableRow2=0x7f080001;
+        public static final int tableRow2=0x7f080002;
         public static final int tableRow3=0x7f080004;
         public static final int tableRow4=0x7f080006;
         public static final int textView1=0x7f080005;
